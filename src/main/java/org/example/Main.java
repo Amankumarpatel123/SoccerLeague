fd147eae2db6aa1b3dade0b9a3643039be75559c
Aman kumar Patel <97835747+Amankumarpatel123@users.noreply.github.com>
2024-04-16 00:15:36 +0530

    error in main need to resolve

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import Exceptions.SoccerExceptions;
+import businesstier.League;
+import persentationtier.LeagueTester;
+import utility.Converter;
+
+import java.text.ParseException;
+import java.util.List;
+
+public class Main{
+    public static  void main(String[] args) throws ParseException {
+        LeagueTester leagueTester= new LeagueTester();
+
+        try {
+            String message= leagueTester.addLeague("laLiga2024","17/04/2024","23/04/2024",5);
+            System.out.println(message);
+        } catch (SoccerExceptions e) {
+           e.printStackTrace();
+        }
+
+        try {
+            List<League> leagueDetail=leagueTester.getLeagueDetails("16/04/2024");
+        } catch (SoccerExceptions e) {
+            e.printStackTrace();
+        }
     }
 }
